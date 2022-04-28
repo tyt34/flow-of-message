ec85cc52ee2fa7ab23834c90599038a6a97d6b91
R N <novoto4in@mail.ru>
2022-04-28 22:29:53 +0300

    deploy

--- front/src/utils/api.js
+++ front/src/utils/api.js
@@ -1,7 +1,15 @@
-const link = 'http://localhost:3001'
+const BASENAME = process.env.REACT_APP_BASENAME
+
+let link
+
+if (BASENAME === undefined) {
+  link = 'https://flow-mess.glitch.me/'
+} else {
+  link = BASENAME
+}
 
 export const getMessages = () => {
-  //console.log(' get from: ', link)
+  console.log(' get from: ', link)
   return fetch(link, {
     method: 'GET',
   })
